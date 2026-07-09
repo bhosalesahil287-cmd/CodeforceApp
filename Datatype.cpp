9de7559cfb208463ad16f66b60697167c26fd2a0
bhosalesahil287 <bhosalesahil287@gmail.com>
2026-07-09 14:11:23 +0530

    save

--- Datatype.cpp
+++ Datatype.cpp
@@ -6,6 +6,7 @@
 #define MODSUB(a,b,MOD1) a+b+MOD1 % MOD1 
 #define MODMUL(l,a,b,MOD1) 1LL * a * b%MOD1 
 #define  P  31      
+#define PI 3.141592653;
 typedef long long ll;
 using namespace std;
 class Codechef{
@@ -39,6 +40,89 @@ class Codechef{
 		   cout<<a<<" - "<<b<<" = "<<a-b<<endl;
 
 	  }
+
+	  static void difference(long long a,long long b,long long c,long long d){
+
+		  long long ans=0;
+
+		  ans=(a*b)-(c*d);
+
+		  cout<<"Difference"<<" = "<<ans;
+	  }
+
+	  static void AreaCircle(double r){
+
+		    double ans=1;
+		    double pi=3.141592653;
+		    cout << fixed << setprecision(9);
+		    ans=pi*(r*r);
+		    cout<<ans;
+
+	  }
+
+	  static void lastsum(long long a,long long b){
+
+		     long long ans=0;
+		     ans=(a%10)+(b%10);
+		     cout<<ans<<endl;
+	  }
+
+	  static void sumation(long long n){
+
+		  long long ans=0;
+		  ans=n*(n+1)/2;
+
+		  cout<<ans;
+	}
+
+    static cellfloorRoundx(int  A,int B){
+
+        cout << "floor " << A << " / " << B << " = " << A / B << "\n";
+        int c = (A + B - 1) / B;
+        cout << "ceil " << A << " / " << B << " = " << c << "\n";
+       	double x = (double)A / B;
+       	int r = (x - (int)x >= 0.5) ? (int)x + 1 : (int)x;
+        cout << "round " << A << " / " << B << " = " << r << "\n";            
+	 }
+
+     static void maxMe(long long m1,long long m2){
+
+	       if(m1>=m2){
+		    cout<<"Yes";
+		}else{
+	            cout<<"No";
+		  }
+	}
+
+       static void multiple(long long n1,long long n2){
+
+	       if(n1%n2==0 || n2%n1==0){
+		    cout<<"Multiples";
+		}else{
+	            cout<<"No Multiples";
+		}
+	 }
+
+        static void maxMin(long long  n1,long long  n2,long long  n3){
+
+		  long long m=max(n1,n2);
+                  long long maxi=max(m,n3);
+		  long long m2=min(n1,n2);
+		  long long mini=min(m2,n3);
+
+		  cout<<mini<<" "<<maxi<<endl;
+
+	
+	}
+
+	static void Bro(string f1,string s1,string f2,string s2){
+
+		    if(f1!=f2 && s1==s2){
+			  cout<<"ARE Brothers";
+		    }else{
+			   cout<<"NOT";
+			 }
+	 }
 	  static void inputarr(){
  
 		 int T;
@@ -68,12 +152,15 @@ class Codechef{
 			  long long longs;
 			  char ch;
 			  float flt;
-			  double d;
+			 
+			  string  a;
+			  string  b;
+			  string  c;
+			  string  d;
 
-			  long long  a;
-			  long long  b;
-			  cin>>a>>b;
-			  calsi(a,b);
+			 cin>>a>>b;
+			 cin>>c>>d;
+                         Bro(a,b,c,d);
 
 		    }	     
 };
